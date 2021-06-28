2b7284688048a26e3c3504725327771abf6054fd
Jonathan Choi <jonchoi11@gmail.com>
2021-06-28 04:59:45 +0000

    the best veg. final attempt.

---
+++ veggies.js
@@ -0,0 +1,35 @@
+const vegetables = [
+  {
+    submitter: 'Old Man Franklin',
+    redness: 10,
+    plumpness: 5
+  },
+  {
+    submitter: 'Sally Tomato-Grower',
+    redness: 2,
+    plumpness: 8
+  },
+  {
+    submitter: 'Hamid Hamidson',
+    redness: 4,
+    plumpness: 3
+  }
+]
+
+const metric = 'redness'
+
+const judgeVegetable = function(vegetables, metric) {
+  let maxPoints = 0;
+  let winner;
+
+  for (let i = 0; i < vegetables.length; i++){
+
+    if (vegetables[i][metric] > maxPoints){
+      maxPoints = vegetables[i][metric];
+      winner = vegetables[i]['submitter'];
+    }
+  }
+  return winner;
+};
+
+judgeVegetable(vegetables, metric);  
